8cf2a7176158a3bec20806f4b65ea35f048bd9b5
Markus Ecker <me@instant.st>
2018-10-28 20:44:45 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="vergeml",
-    version="0.1.2",
+    version="0.1.3",
     author="Markus Ecker",
     author_email="markus.ecker@gmail.com",
     description="Machine Learning Environment",
