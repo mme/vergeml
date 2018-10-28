2ce5b83e3df2e489ce4f1661023cc140859e436c
Markus Ecker <me@instant.st>
2018-10-28 16:21:14 +0100

    add __init__.py to modules

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -13,7 +13,7 @@ setup(
     license="MIT",
     keywords="ai deep learning",
     url="http://github.com/vergeml/vergeml",
-    packages=['vergeml'],
+    packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
