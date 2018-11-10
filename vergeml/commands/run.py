896a81dd4c6ea4c2b7c3a9ae5a82baea4c146459
Markus Ecker <me@instant.st>
2018-11-10 18:34:01 +0100

    clean up

--- vergeml/commands/run.py
+++ vergeml/commands/run.py
@@ -1,12 +1,5 @@
 from vergeml.command import command, CommandPlugin
-from vergeml.plugins import PLUGINS
 from vergeml.option import option
-from vergeml.utils import VergeMLError
-import shutil
-import os.path
-import zipfile
-import tarfile
-
 
 @command('run', descr="Run a service or external program.")
 @option('service', type=str, subcommand='vergeml.run')
